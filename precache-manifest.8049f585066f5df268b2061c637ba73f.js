d0e956db5c0c48cfd0e8e7b186eb44d0e0fb1e36
Christina Starr <christinastarr19@gmail.com>
2020-08-03 23:29:49 -0400

    Updates

---
+++ precache-manifest.8049f585066f5df268b2061c637ba73f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "438e956d952dc84627986bc3c788e2cb",
+    "url": "/userdirectory-homework-19/index.html"
+  },
+  {
+    "revision": "74d99adbc99e128f9833",
+    "url": "/userdirectory-homework-19/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "371c30bfd75d9641470b",
+    "url": "/userdirectory-homework-19/static/js/2.9097a5c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/userdirectory-homework-19/static/js/2.9097a5c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74d99adbc99e128f9833",
+    "url": "/userdirectory-homework-19/static/js/main.97773ca2.chunk.js"
+  },
+  {
+    "revision": "f97b067257c3ff5ca8a3",
+    "url": "/userdirectory-homework-19/static/js/runtime-main.c660492c.js"
+  }
+]);
